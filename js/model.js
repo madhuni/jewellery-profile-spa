dc17ebcb4ae86a522502ff547bb6b4bef07dd9f7
Kanishka Madhuni <kanishkamohanmadhuni@gmail.com>
2017-08-25 20:11:33 +0530

    feat: Added model for the project

---
+++ js/model.js
@@ -0,0 +1,65 @@
+var user = {
+	profile: {
+        name: "Kanishka Mohan Madhuni",
+        age: "24",
+        sex: "Male",
+        contactInfo: "+91-8884669993"
+    },
+    collections: [{
+        bracelets: [{
+            type: "Bangle Bracelet",
+            material: "Gold",
+            imageUrl: "images/bracelets/gold-cuff-bracelet.jpg"
+        }, {
+            type: "Cuff Bracelet",
+            material: "Silver",
+            imageUrl: "images/bracelets/silver-charm-bracelet.jpg"
+        }, {
+            type: "Link Bracelet",
+            material: "Diamond",
+            imageUrl: "images/bracelets/diamond-link-bracelet.jpg"
+        }]
+    }, {
+        earrings: [{
+            type: "Jhumkis",
+            material: "Gold",
+            imageUrl: "images/earrings/gold-jhumki.jpg"
+        }, {
+            type: "Chandelier Earrings",
+            material: "Diamond",
+            imageUrl: "images/earrings/diamond-earring.jpg"
+        }, {
+            type: "Huggies Earrings",
+            material: "Silver",
+            imageUrl: "images/earrings/silver-huggeis.jpg"
+        }]
+    }, {
+        rings: [{
+            type: "Marriage Ring",
+            material: "Gold",
+            imageUrl: "images/rings/gold-marriage-ring.jpg"
+        }, {
+            type: "Eternity Ring",
+            material: "Diamond",
+            imageUrl: "images/rings/diamond-eternity-ring.jpeg"
+        }, {
+            type: "Knuckle Ring",
+            material: "Silver",
+            imageUrl: "images/rings/silver-knuckle-ring.jpg"
+        }]
+    }, {
+    	necklaces: [{
+    		type: "Opera Necklace",
+    		material: "Gold",
+    		imageUrl: "images/necklaces/gold-opera-necklace.jpg"
+    	}, {
+    		type: "Collar Necklace",
+    		material: "Diamond",
+    		imageUrl: "images/necklaces/diamond-collar-necklace.jpg"
+    	}, {
+    		type: "Thread Necklace",
+    		material: "Silver",
+    		imageUrl: "images/necklaces/silver-thread-nacklace.jpg"
+    	}]
+    }]
+};
